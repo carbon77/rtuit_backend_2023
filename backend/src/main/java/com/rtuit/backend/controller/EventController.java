0b3a593236c75b9f91aacbd7af56ea056eed21ef
Igor <zakatov0303@gmail.com>
2023-05-14 16:26:25 +0300

    feat: addCategoryToEvent method
    Created addCategoryToEvent method in EventService and EventController

--- backend/src/main/java/com/rtuit/backend/controller/EventController.java
+++ backend/src/main/java/com/rtuit/backend/controller/EventController.java
@@ -1,14 +1,42 @@
 package com.rtuit.backend.controller;
 
 import com.rtuit.backend.model.Event;
+import com.rtuit.backend.model.EventCategory;
+import com.rtuit.backend.service.EventCategoryService;
 import com.rtuit.backend.service.EventService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/events")
 public class EventController extends CrudController<Event, Integer> {
-    public EventController(EventService service) {
+    private final EventCategoryService eventCategoryService;
+    private final EventService eventService;
+
+    public EventController(EventService service, EventCategoryService eventCategoryService) {
         super(service);
+        this.eventCategoryService = eventCategoryService;
+        this.eventService = service;
+    }
+
+    @PostMapping("/{eventId}/category")
+    public void addCategoryToEvent(
+            @PathVariable Integer eventId,
+            @RequestBody Integer categoryId
+    ) {
+        EventCategory category = eventCategoryService.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "There is no category with id=" + categoryId
+                ));
+
+        Event event = eventService.findById(eventId)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "There is no event with id=" + eventId
+                ));
+
+        eventService.addCategoryToEvent(event, category);
     }
 }
--- backend/src/main/java/com/rtuit/backend/service/EventService.java
+++ backend/src/main/java/com/rtuit/backend/service/EventService.java
@@ -1,12 +1,21 @@
 package com.rtuit.backend.service;
 
 import com.rtuit.backend.model.Event;
+import com.rtuit.backend.model.EventCategory;
 import com.rtuit.backend.repository.EventRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EventService extends CrudService<Event, Integer> {
+    private final EventRepository eventRepository;
+
     public EventService(EventRepository repository) {
         super(repository);
+        this.eventRepository = repository;
+    }
+
+    public void addCategoryToEvent(Event event, EventCategory category) {
+        event.setEventCategory(category);
+        eventRepository.save(event);
     }
 }
